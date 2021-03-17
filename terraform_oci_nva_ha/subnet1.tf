99429cec04baec8c0b232438ee85454493bdb996
BaptisS <56864846+BaptisS@users.noreply.github.com>
2021-03-17 18:15:46 +0100

    Update subnet1.tf

--- terraform_oci_nva_ha/subnet1.tf
+++ terraform_oci_nva_ha/subnet1.tf
@@ -2,7 +2,7 @@ resource "oci_core_subnet" "BSPublicSubnet" {
   cidr_block = "192.168.168.0/27"
   display_name = "DMZ-SN"
   dns_label = "dmz"
-  compartment_id = oci_identity_compartment.BSCompartment.id
+  compartment_id = var.compartment_ocid
   vcn_id = oci_core_virtual_network.BSVCN.id
   route_table_id = oci_core_route_table.BSRouteTableViaIGW.id
   dhcp_options_id = oci_core_dhcp_options.BSDhcpOptions1.id
