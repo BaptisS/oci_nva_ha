71262d8d4bc0fedd9015232b38074ca1bf495575
BaptisS <56864846+BaptisS@users.noreply.github.com>
2021-03-17 18:10:26 +0100

    Update vcn.tf

--- terraform_oci_nva_ha/vcn.tf
+++ terraform_oci_nva_ha/vcn.tf
@@ -10,8 +10,3 @@ data "oci_identity_availability_domains" "ADs" {
   compartment_id = var.tenancy_ocid
 }
 
-# Gets the Id of a specific OS Images
-data "oci_core_images" "OSImageLocal" {
-  compartment_id = var.compartment_ocid
-  display_name   = var.OsImage
-}
