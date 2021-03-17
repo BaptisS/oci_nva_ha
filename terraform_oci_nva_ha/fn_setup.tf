e8765160f3c9ffd78e93f5d61a428b172705f1b3
BaptisS <56864846+BaptisS@users.noreply.github.com>
2021-03-17 17:54:16 +0100

    Update fn_setup.tf

--- terraform_oci_nva_ha/fn_setup.tf
+++ terraform_oci_nva_ha/fn_setup.tf
@@ -7,17 +7,17 @@ resource "null_resource" "BSFnSetup" {
 
   provisioner "local-exec" {
     command = "fn build"
-    working_dir = "functions\oci_nva_ha"
+    working_dir = "functions/oci_nva_ha"
   }
 
   provisioner "local-exec" {
     command = "image=$(docker images | grep nva_ha | awk -F ' ' '{print $3}') ; docker tag $image ${var.ocir_docker_repository}/${var.ocir_namespace}/${var.ocir_repo_name}/nva_ha:0.0.1"
-    working_dir = "functions\oci_nva_ha"
+    working_dir = "functions/oci_nva_ha"
   }
 
   provisioner "local-exec" {
     command = "docker push ${var.ocir_docker_repository}/${var.ocir_namespace}/${var.ocir_repo_name}/nva_ha:0.0.1"
-    working_dir = "functions\oci_nva_ha"
+    working_dir = "functions/oci_nva_ha"
   }
 
 
