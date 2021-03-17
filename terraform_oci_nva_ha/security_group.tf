b42a4356c4b915bdba4484f0c30f9aeb0673d6f3
BaptisS <56864846+BaptisS@users.noreply.github.com>
2021-03-17 18:16:16 +0100

    Update security_group.tf

--- terraform_oci_nva_ha/security_group.tf
+++ terraform_oci_nva_ha/security_group.tf
@@ -1,5 +1,5 @@
 resource "oci_core_network_security_group" "BSSSHSecurityGroup" {
-    compartment_id = oci_identity_compartment.BSCompartment.id
+    compartment_id  = var.compartment_ocid
     display_name = "BSSSHSecurityGroup"
     vcn_id = oci_core_virtual_network.BSVCN.id
 }
